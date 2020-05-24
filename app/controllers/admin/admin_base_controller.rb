74fbf21077eee18d774a325334ec6f63f205dab6
quangphu <nguyen.quang.phu@sun-asterisk.com>
2020-05-24 17:01:43 +0700

    fix linh tinh

--- app/controllers/admin/admin_base_controller.rb
+++ app/controllers/admin/admin_base_controller.rb
@@ -1,11 +1,20 @@
 module Admin
   class AdminBaseController < ApplicationController
     before_action :notification
+    before_action :authenticate_user!
+    before_action :check_is_admin
 
     layout "application_admin"
 
     def notification
       @notifications = Notification.includes(:user).order_created_at_desc.limit(5)
+    end
+
+    private
+    def check_is_admin
+      if user_signed_in? && !current_user.is_admin?
+        redirect_to root_path, :warning => t("not_authen")
+      end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       super
     else
-      redirect_to signin_path, :warning => 'if you want to add a notice'
+      redirect_to signin_path, :warning => t("must_login")
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
