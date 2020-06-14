5d8d6ffc39f8a00e10129fd2ae36411ebfe45d30
quangphu <nguyen.quang.phu@sun-asterisk.com>
2020-06-14 21:21:35 +0700

    csac

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -6,4 +6,11 @@ class OrderDetail < ApplicationRecord
 
   scope :by_order, -> (order_id){where order_id: order_id}
   scope :top_product, -> {group(:book_id).order('sum(quantity) desc').limit(3).pluck(:book_id)}
+
+  after_save :update_quantity_product
+
+  private
+  def update_quantity_product
+    book.update quantity: (book.quantity - quantity.to_i)
+  end
 end
